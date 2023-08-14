94751dd24941abecd012c54badd9ef80967a1896
Sergey Smirnov <smirnov.sege@gmail.com>
2023-08-14 20:47:36 +0300

    add people fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import getPeople from "@/api/getPeople";
 import styles from "./page.module.css";
 
-export default function Home() {
+export const metadata = {
+  title: "Star Wars characters library",
+};
+
+export default async function Home() {
+  const people = await getPeople();
+  console.log(people);
   return <main className={styles.main}>here we go</main>;
 }
