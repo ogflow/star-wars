58ddaa7293d08e9ba538abc884302b23475e092a
Sergey Smirnov <smirnov.sege@gmail.com>
2023-08-16 22:36:33 +0300

    open person page in a modal

--- app/people/people-list.tsx
+++ app/people/people-list.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { Box, Button, Grid, Spinner } from "grommet";
+import { getPerson } from "@/api/people";
+import { Box, Button, Grid, Layer, Spinner } from "grommet";
 import Link from "next/link";
+import { MouseEvent, useState } from "react";
 import useSWRInfinite, { SWRInfiniteKeyLoader } from "swr/infinite";
+import Profile from "./[id]/profile";
 import PersonCard from "./person-card";
 
 const getKey: SWRInfiniteKeyLoader = (pageIndex, prevPageData) => {
@@ -11,6 +14,7 @@ const getKey: SWRInfiniteKeyLoader = (pageIndex, prevPageData) => {
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function PeopleList() {
+  const [modalPerson, setModalPerson] = useState<Person | boolean>(false);
   const { data, error, isLoading, size, setSize, mutate } = useSWRInfinite(
     getKey,
     fetcher,
@@ -28,28 +32,57 @@ export default function PeopleList() {
   const isEmpty = data?.[0]?.length === 0;
   const hasMore = people?.length < data?.[0]?.count;
 
+  const handleModalOpen = async (e: MouseEvent, personId: string) => {
+    e.preventDefault();
+    setModalPerson(true);
+    const res = await getPerson(personId);
+
+    setModalPerson(res);
+    window.history.pushState(null, "", "/people/" + personId);
+  };
+
+  const handleModalClose = () => {
+    setModalPerson(false);
+    window.history.back();
+  };
+
   return (
-    <Grid columns={{ count: "fit", size: "480px" }} gap="medium">
-      {isEmpty && <p>No people live here!</p>}
-      {people?.map((person) => {
-        const href = `/people/${person.url.split("/")[5]}`;
-        return (
-          <Link key={person.url} href={href}>
-            <PersonCard person={person} />
-          </Link>
-        );
-      })}
-      {error && (
-        <Box>
-          <p>Failed to load</p>
-          <Button onClick={() => mutate()}>Retry</Button>
-        </Box>
+    <>
+      <Grid columns={{ count: "fit", size: "480px" }} gap="medium">
+        {isEmpty && <p>No people live here!</p>}
+        {people?.map((person) => {
+          const personId = person.url.split("/")[5];
+          return (
+            <Link
+              key={person.url}
+              href={`/people/${personId}`}
+              onClick={(e) => handleModalOpen(e, personId)}
+            >
+              <PersonCard person={person} />
+            </Link>
+          );
+        })}
+        {error && (
+          <Box>
+            <p>Failed to load</p>
+            <Button onClick={() => mutate()}>Retry</Button>
+          </Box>
+        )}
+        {hasMore && !error && (
+          <Button onClick={() => setSize(size + 1)} busy={isLoadingMore}>
+            Load more
+          </Button>
+        )}
+      </Grid>
+      {modalPerson && (
+        <Layer onEsc={handleModalClose} onClickOutside={handleModalClose} modal>
+          {modalPerson === true ? (
+            <Spinner size="medium" />
+          ) : (
+            <Profile person={modalPerson} />
+          )}
+        </Layer>
       )}
-      {hasMore && !error && (
-        <Button onClick={() => setSize(size + 1)} busy={isLoadingMore}>
-          Load more
-        </Button>
-      )}
-    </Grid>
+    </>
   );
 }
