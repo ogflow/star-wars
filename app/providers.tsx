4a5738ffec37d829f93babaadef4feff1c52d98f
Sergey Smirnov <smirnov.sege@gmail.com>
2023-08-19 13:34:44 +0300

    basic color palette

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
         shouldRetryOnError: false,
       }}
     >
-      <Grommet theme={theme} full>
+      <Grommet theme={theme} themeMode="dark" full>
         {children}
       </Grommet>
     </SWRConfig>
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,3 +1,42 @@
+const palette = {
+  brand: "#FFE81F",
+  background: {
+    primary: "#000000",
+    front: "#1d1f24",
+    contrast: "#2a2d36",
+  },
+  text: {
+    primary: "#FFFFFF",
+    weak: "#b1b4bd",
+    xweak: "#83868f",
+  },
+  border: "#3f424a",
+  focus: "#0044ff",
+};
+
 export const theme = {
-  global: {},
+  rounding: 8,
+  spacing: 28,
+  global: {
+    font: {
+      family: "Helvetica",
+      size: "21px",
+      height: "28px",
+      maxWidth: "588px",
+    },
+    colors: {
+      brand: palette.brand,
+      background: palette.background.primary,
+      "background-back": palette.background.primary,
+      "background-front": palette.background.front,
+      "background-contrast": palette.background.contrast,
+      text: palette.text.primary,
+      "text-strong": palette.text.primary,
+      "text-weak": palette.text.weak,
+      "text-xweak": palette.text.xweak,
+      border: palette.border,
+      focus: palette.focus,
+      control: "brand",
+    },
+  },
 };
