96aadf7fdfa0729dd7cfcc0a921c4f33f8cfa103
Sergey Smirnov <smirnov.sege@gmail.com>
2023-08-20 22:44:39 +0300

    bg video for start page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Header from "@/compoments/header";
 import { Button, Heading, Page, PageContent } from "grommet";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export const metadata = {
   title: "Star Wars App",
@@ -7,14 +9,26 @@ export const metadata = {
 
 export default async function Home() {
   return (
-    <Page kind="wide">
-      <Header />
-      <PageContent as="main" align="center">
-        <Heading level="1" margin="xlarge" textAlign="center" size="large">
-          Discover more about your favorite Star Wars character
-        </Heading>
-        <Button primary label="Begin your journey" size="xlarge" />
-      </PageContent>
-    </Page>
+    <>
+      <Page kind="wide" overflow="hidden" className={styles.page}>
+        <Header />
+        <PageContent
+          as="main"
+          align="center"
+          justify="center"
+          className={styles.content}
+        >
+          <Heading level="1" margin="large" textAlign="center" size="large">
+            Discover more about your favorite Star Wars character
+          </Heading>
+          <Link href="/people">
+            <Button primary label="Begin your journey" size="2xl" />
+          </Link>
+        </PageContent>
+      </Page>
+      <video autoPlay loop controls={false} className={styles.video}>
+        <source src="/hyperspace.mp4" />
+      </video>
+    </>
   );
 }
